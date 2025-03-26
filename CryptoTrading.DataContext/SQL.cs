00664021dfbfa3767bc851dbd3ead56c54994375
tothe98 <totherikjanos@gmail.com>
2025-03-26 23:26:31 +0100

    Add project files.

---
+++ CryptoTrading.DataContext/SQL.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTrading.DataContext
+{
+    public class SQL: DbContext
+    {
+        public SQL(DbContextOptions options) : base(options) { }
+    }
+}
