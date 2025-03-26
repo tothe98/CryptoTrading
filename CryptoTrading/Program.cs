857447e6e239aa0b665e33eaee56e3bfd5673bf5
tothe98 <totherikjanos@gmail.com>
2025-03-26 23:37:00 +0100

    Add Swagger

--- CryptoTrading/Program.cs
+++ CryptoTrading/Program.cs
@@ -4,11 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CryptoTrading API v1"));
 }
 
 app.UseHttpsRedirection();
